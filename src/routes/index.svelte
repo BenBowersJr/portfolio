033398fe8604cf55f666594f3dd58bd9b4e7ce10
Benjamin Bowers <benbowersjr.22@gmail.com>
2022-07-25 18:07:23 -0400

    changed contact link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,7 +24,7 @@
     <div transition:blur={{ duration: 1000}} class="landingpage">
       <LandingPage 
         on:work={()=>navigate('Work')} 
-        on:contact={()=>navigate('Contact')}>
+        on:contact={()=>navigate('Work')}>
       </LandingPage>
     </div>
   {/if}
